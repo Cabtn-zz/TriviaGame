49c5a1e2e7f69fa4e008db9b8f36cb7a5ed98e32
Abtin Ravanipour <Abtin Ravanipour>
2017-02-26 18:43:51 -0800

    console log baby

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,11 +8,13 @@ var currentQuestion = 0;
 var triviaGame = [
 {
 	q1: "Who is your daddy and what does he do?",
-	answers: ["Arnold S ", "Jake J ", "Matt Mc "]
+	answers: ["Arnold S", "Jake J", "Matt Mc"],
+	rightAnswer: "Arnold S",
 },
 {
 	q1: "How are you?",
-	answers: ["Great ", "Average ", "test "],
+	answers: ["Great", "Average", "test"],
+	rightAnswer: "Great",
 }
 ];
 
@@ -25,9 +27,29 @@ $("#start").on("click", function startGame(){
 function nextQuestion () {
 	if (count < triviaGame.length) {
 		$("#questions").html(triviaGame[count].q1);
-		$("#answers").html(triviaGame[count].answers)
+		$("#answers").html(" " + triviaGame[count].answers + " ");
+		console.log(triviaGame[count].rightAnswer);
+		checkAnswer();
 		count++
 		
  }
 }
+$(triviaGame.answers).on("click", function chooseAnswer(){
+	console.log(this);
+	});
+
+function checkAnswer() {
+	// This if statement doesn't work but the else does. Fix chooseAnswer
+	if(count === triviaGame[count].rightAnswer){
+		console.log("You're right!");
+		correctAnswers++;
+	}
+	else{
+		console.log("The correct answer was " + triviaGame[count].rightAnswer);
+		wrongAnswers++
+	}
+	console.log("Correct: " + correctAnswers)
+	console.log("Incorrect: " + wrongAnswers)
+}
+
 

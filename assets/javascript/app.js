f61a4bba15e67e50dfa4b4aadea7faa0d26a6a35
Abtin Ravanipour <Abtin Ravanipour>
2017-02-26 16:31:36 -0800

    fixed it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,28 +3,31 @@ var score = 0;
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var count = 0;
-var questions = [q1];
+var currentQuestion = 0;
 
-
-var triviaGame = {
-
+var triviaGame = [
+{
 	q1: "Who is your daddy and what does he do?",
-	q2: "I am your daddy",
-	q3: "No you are not",
-	answers: ["Arnold S", "Jake J", "Matt Mc"],
-	
+	answers: ["Arnold S ", "Jake J ", "Matt Mc "]
+},
+{
+	q1: "How are you?",
+	answers: ["Great ", "Average ", "test "],
 }
+];
 
 $("#start").on("click", function startGame(){
 	console.log("button works");
-	// $("#q1").html(questions[count])
-	// nextQuestion();
-	console.log(triviaGame.questions[this]);
+	$("button").fadeOut();
+	currentQuestion	= setInterval(nextQuestion, 2000);
 });
 
 function nextQuestion () {
-	if (count < questions.length - 1) {
-		count = count++
-		console.log(triviaGame.q1);
+	if (count < triviaGame.length) {
+		$("#questions").html(triviaGame[count].q1);
+		$("#answers").html(triviaGame[count].answers)
+		count++
+		
  }
 }
+

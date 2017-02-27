38d1d2f759741191d2c43083e9d482ac85885e11
Abtin Ravanipour <Abtin Ravanipour>
2017-02-26 20:07:26 -0800

    Just need to make the buttons work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,21 +7,36 @@ var currentQuestion = 0;
 
 var triviaGame = [
 {
-	q1: "Who is your daddy and what does he do?",
-	answers: ["Arnold S", "Jake J", "Matt Mc"],
-	rightAnswer: "Arnold S",
+	q1: "What has roots as nobody sees, is taller than trees, Up, up it goes, and yet never grows",
+	answers: ["Math", "Skyscraper", "Family", "Mountain"],
+	rightAnswer: "Mountain",
 },
 {
-	q1: "How are you?",
-	answers: ["Great", "Average", "test"],
-	rightAnswer: "Great",
-}
+	q1: "Who is the White Whale",
+	answers: ["Narwhals", "Jack Sparrow", "Moby Dick", "Patrick"],
+	rightAnswer: "Moby Dick",
+},
+{
+	q1: "What is 2 + 2?",
+	answers: ["4", "22", "Math", "Boolean"],
+	rightAnswer: "22",
+},
+{
+	q1: "Who created JavaScript?",
+	answers: ["Michael B", "GT", "Darwin", "Patrick"],
+	rightAnswer: "Darwin",
+},
+{
+	q1: "Who is your daddy what does he do?",
+	answers: ["Arnold S", "Detective John Kimble", "A Baker", "Darth Vader"],
+	rightAnswer: "Arnold S",
+},
 ];
 
 $("#start").on("click", function startGame(){
 	console.log("button works");
 	$("button").fadeOut();
-	currentQuestion	= setInterval(nextQuestion, 2000);
+	currentQuestion	= setInterval(nextQuestion, 5000);
 });
 
 function nextQuestion () {
@@ -31,7 +46,7 @@ function nextQuestion () {
 		console.log(triviaGame[count].rightAnswer);
 		checkAnswer();
 		count++
-		
+		gameOver();
  }
 }
 $(triviaGame.answers).on("click", function chooseAnswer(){
@@ -52,4 +67,11 @@ function checkAnswer() {
 	console.log("Incorrect: " + wrongAnswers)
 }
 
+function gameOver(){
+	if(count === triviaGame.length){
+		console.log("GAME OVER");
+		count = 0;
+		return;
+	}
+}
 
